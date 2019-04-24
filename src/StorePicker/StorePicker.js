5941f6235e848f80d36667ac244b86bff4dca837
Kevin Krato <kratok08@gmail.com>
2019-04-24 09:39:40 -0600

    Create ref and push

--- src/StorePicker/StorePicker.js
+++ src/StorePicker/StorePicker.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { getFunName } from "../utilities";
 
-const StorePicker = () => {
-  return (
-    <form>
-      <h1>Welcome</h1>
-      <input type="text" placeholder="enter store here" />
-    </form>
-  );
-};
+class StorePicker extends React.Component {
+  myInput = React.createRef();
+  goToStore = e => {
+    e.preventDefault();
+    this.props.history.push(`store/${this.myInput.current.value}`);
+    console.log(this.myInput.current.value);
+  };
+  render() {
+    return (
+      <form onSubmit={this.goToStore}>
+        <h1>Welcome</h1>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Enter store here"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store</button>
+      </form>
+    );
+  }
+}
 
 export default StorePicker;
