4b42ad3c1dab3e43a51278ade7ccde4a37005619
Kevin Krato <kratok08@gmail.com>
2019-04-24 08:47:54 -0600

    Build data struct

---
+++ src/sample-shoes.js
@@ -0,0 +1,20 @@
+const shoes = {
+  shoe1: {
+    name: "Nike Vaporfly",
+    type: "Running shoe",
+    desc:
+      "Built to meet the exacting needs of world-class marathoners, Nike Vaporfly 4% Flyknit is designed for record-breaking speed.",
+    image: "https://i.imgur.com/QvpQaS8.jpg",
+    shown: "Bright Crimson/Sapphire/White/Black",
+    rating: 4.1
+  }
+  // shoe1: {
+  //   name: ,
+  //   type: ,
+  //   desc:,
+  //   shown: ,
+  //   rating: 4.1
+  // }
+};
+
+export default shoes;
