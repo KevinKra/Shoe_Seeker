779d6307464576d9000925af9ca18049bd676f9e
Kevin Krato <kratok08@gmail.com>
2019-04-24 08:47:38 -0600

    Create StorePicker component

---
+++ src/StorePicker/StorePicker.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const StorePicker = () => {
+  return (
+    <form>
+      <h1>Welcome</h1>
+      <input type="text" placeholder="enter store here" />
+    </form>
+  );
+};
+
+export default StorePicker;
