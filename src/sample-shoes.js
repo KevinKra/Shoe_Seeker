578247076e19f87099d61f9d1bcb2793e712a33f
Kevin Krato <kratok08@gmail.com>
2019-04-24 11:33:34 -0600

    Add additional obj

--- src/sample-shoes.js
+++ src/sample-shoes.js
@@ -7,6 +7,15 @@ const shoes = {
     image: "https://i.imgur.com/QvpQaS8.jpg",
     shown: "Bright Crimson/Sapphire/White/Black",
     rating: 4.1
+  },
+  shoe2: {
+    name: "Nike Wisp",
+    type: "track shoe",
+    desc:
+      "Something awesome, Nike Wisp is designed for record-breaking wisping.",
+    image: "https://i.imgur.com/QvpQaS8.jpg",
+    shown: "Dark Crimson/White/Black",
+    rating: 5.1
   }
   // shoe1: {
   //   name: ,
